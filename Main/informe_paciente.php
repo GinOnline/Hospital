fed38821f6e8afd48c72c8cafbd91b7d20829254
GinOnline <est.spadoni.gino@latecnicalf.com.ar>
2022-11-18 00:23:09 -0300

    Historia clinica

--- Main/informe_paciente.php
+++ Main/informe_paciente.php
@@ -225,7 +225,7 @@ if (!isset($usuario)) {
                 <!--Peticiones -->
                 <h2 class="col-6 btn_title">Informe Paciente <?php echo ' ' . $fila["name_paciente"]. ' ' .$fila["surname"];?></h2>
                 <a href = "informe.pdf" download="informe.pdf" class="buttonDownload col-2" style="margin-right:2%;">Descargar PDF</a>
-                <a href = "historial.php?seachbar_input=<?php echo $fila["name_paciente"];?>" class="buttonDownload col-2">Historial clinico</a>
+                <a href = "historial.php?seachbar_input=<?php echo $fila["name_paciente"];?>" style = "position:absolute; bottom:25px; right:50px; border-radius: 15px" class="btn btn-primary col-2">Historial clinico</a>
             </div>
         </div>
         <br>
